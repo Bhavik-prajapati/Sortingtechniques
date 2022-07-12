f73c6d3cffb2521d9a3b0f2d759cc7f529457f95
Bhavik-prajapati <109121443+Bhavik-prajapati@users.noreply.github.com>
2022-07-12 09:05:13 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -10,25 +10,12 @@ int main() {
         cin>>arr[i];
     }
     
-    int counter=1;
-    
-    while(counter<=n){
-        
-    for(int i=0;i<n;i++){
-        
-        for(int i=0;i<n-counter;i++){
-            
-            if(arr[i]>arr[i+1]){
-                int temp=arr[i];
-                arr[i]=arr[i+1];
-                arr[i+1]=temp;
-            }
-
-        }
-        counter++;
-        
-     }
-    }
+     int i, j;
+    for (i = 0; i < n - 1; i++)
+  
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
     
      for(int i=0;i<n;i++){
         cout<<arr[i]<<endl;
